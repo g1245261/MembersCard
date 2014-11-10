fc5c6852a65b93b794b99776304accd395586ccb
Yamashita Shinya <yamage.mail@gmail.com>
2014-11-10 11:52:51 +0900

    例題4.7 MembersCard.javaのメソッド追加に伴う例プログラムの追加

--- MembersCard/src/MembersCard/Example0302.java
+++ MembersCard/src/MembersCard/Example0302.java
@@ -2,9 +2,12 @@ package MembersCard;
 
 public class Example0302 {
 	public static void main(String[] args){
+		int points;
 		MembersCard m1;
 		m1 = new MembersCard();
 		m1.init(1, "山下", 100);
+		points = m1.usePoints(10);
+		System.out.println("使用したポイントは" + points + "点です.");
 		
 		/*
 		m1.setMemberId(1);
