41eac45a37f8092c3d5ce3a4c6228d20ef71e980
Yamashita Shinya <yamage.mail@gmail.com>
2014-11-10 11:19:41 +0900

    例題4.5 MembersCard.java変更に伴う例プログラムの変更

--- MembersCard/src/MembersCard/Example0302.java
+++ MembersCard/src/MembersCard/Example0302.java
@@ -4,9 +4,15 @@ public class Example0302 {
 	public static void main(String[] args){
 		MembersCard m1;
 		m1 = new MembersCard();
+		m1.setMemberId(1);
+		m1.setName("山下");
+		m1.setPoints(100);
+		
+		/*
 		m1.memberId = 1;
 		m1.name = "山下";
 		m1.points = 100;
+		*/
 		
 		m1.show();
 	}
