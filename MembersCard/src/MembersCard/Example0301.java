2a13e295d12b1aca1193fe2566adad904a045c88
Yamashita Shinya <yamage.mail@gmail.com>
2014-11-10 10:58:27 +0900

    例題4.2 MembersCard.javaを使った例プログラムの追加

--- MembersCard/src/MembersCard/Example0301.java
+++ MembersCard/src/MembersCard/Example0301.java
@@ -3,7 +3,15 @@ package MembersCard;
 public class Example0301 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		MembersCard m1;
+		m1 = new MembersCard();
+		m1.memberId = 1;
+		m1.name = "山下";
+		m1.points = 100;
+		
+		System.out.println("会員番号は" + m1.memberId + "番");
+		System.out.println("会員氏名は" + m1.name);
+		System.out.println("累計ポイントは" + m1.points + "点です.");
 
 	}
 
